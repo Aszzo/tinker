26d02f37454396392e919982072f1987dbc4dfe5
longquan.bing <longquan.bing@asiainnovations.com>
2020-05-25 19:00:58 +0800

    add npm

--- main.go
+++ main.go
@@ -3,60 +3,51 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-	"os"
-
 	f "github.com/Aszzo/tinker/file"
+	"log"
 )
 var (
-	h bool
 	i string
 	q int
 )
 
 func init()  {
-
-	flag.BoolVar(&h, "h", false, "this help")
 	flag.StringVar(&i, "i", "", "目标文件所在的目录|目标文件路径")
 	flag.IntVar(&q, "q", 80, "压缩图片的质量（0-100）")
 }
-func usage()  {
-	fmt.Fprint(os.Stderr, `version/1.0.0
-Usage: tinker [-i input] [-q quality]
-Options:
-`)
-flag.PrintDefaults()
-}
 func main()  {
 	flag.Parse()
-	if h {
-		usage()
-		return
-	}
+
 	var fileList []string
 	// 首先判断是文件还是文件夹
-	isDir := f.IsDir("file")
+	isDir := f.IsDir(i)
 
 	if isDir {
 		// 如果是文件夹，遍历文件夹,获取文件夹内的所有文件
 		f.RangeDir(i, &fileList)
+
+		for _, path := range fileList {
+			resize(path)
+		}
+	} else {
+		// 如果是图像文件直接压缩
+		resize(i)
 	}
-	for _, path := range fileList{
-		mime, err := f.GetFileContentType(path)
-		if err != nil{
-			log.Fatal(err)
-		}
+}
+func resize(path string)  {
+	mime, err := f.GetFileContentType(path)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		switch mime {
-			case "image/jpeg":
-				fmt.Println("正在压缩文件：", path)
-				f.ResizeJpg(path, q)
-			case "image/png":
-				fmt.Println("正在压缩文件：", path)
-				f.ResizePng(path, q)
-			default:
-				fmt.Println(path + "不是jpeg或者png格式的文件")
-		}
-
+	switch mime {
+	case "image/jpeg":
+		fmt.Println("正在压缩文件：", path)
+		f.ResizeJpg(path, q)
+	case "image/png":
+		fmt.Println("正在压缩文件：", path)
+		f.ResizePng(path, q)
+	default:
+		fmt.Println(path + "不是jpeg或者png格式的文件")
 	}
 }
---
+++ npm/tinker/lib/tinker.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const program = require('commander');
+const pkgConfig = require('../package');
+const child_process = require('child_process');
+const path = require('path');
+let  flags = [];
+program
+    .version(pkgConfig.version, '-v, --version')
+    .usage('<command> [options]')
+    .option('-i, --input <value>', '目标文件所在的目录|目标文件路径')
+    .option('-q, --quality <number>', '压缩图片的质量(0-100)', 80)
+;
+program.parse(process.argv);
+
+if (program.input) {
+    flags = [...flags, ...['-i', program.input]]
+}
+flags = [...flags, ...['-q', program.quality]];
+
+const tinker = path.join(__dirname, '..', 'bin', 'tinker');
+
+child_process.execFile(tinker, flags, function (err, data) {
+    if (err) {
+        console.error(err)
+    }
+    console.log(data)
+})
